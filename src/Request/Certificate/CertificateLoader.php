3427f0c02b62e824e3d3a1d43febb5f418b797d8
Ralf Eggert <ralf@travello.com>
2017-07-13 14:38:18 +0200

    Added license to files

--- src/Request/Certificate/CertificateLoader.php
+++ src/Request/Certificate/CertificateLoader.php
@@ -3,6 +3,7 @@
  * PHP Library for Amazon Alexa Skills
  *
  * @author     Ralf Eggert <ralf@travello.de>
+ * @license    http://opensource.org/licenses/MIT The MIT License (MIT)
  * @link       https://github.com/travello-gmbh/amazon-alexa-skill-library
  * @link       https://www.travello.de/
  *
